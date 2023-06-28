93a59374d74b0cd3498be1dee78336f4936c8612
Andreas Bigger <abigger87@gmail.com>
2023-06-28 13:26:11 -0400

    FaultResponder initial implementation

---
+++ op-challenger/fault/responder.go
@@ -0,0 +1,172 @@
+package fault
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
+	"github.com/ethereum-optimism/optimism/op-service/txmgr"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/log"
+)
+
+// faultResponder implements the [Responder] interface to send onchain transactions.
+type faultResponder struct {
+	log log.Logger
+
+	shutdownCtx       context.Context
+	cancelShutdownCtx context.CancelFunc
+
+	// Transaction manager and configuration.
+	txMgr                  txmgr.TxManager
+	MaxPendingTransactions uint64
+
+	// Fault Dispute Game contract.
+	fdgAddr               *common.Address
+	fdgContractTransactor *bindings.FaultDisputeGameTransactor
+	fdgContractCaller     *bindings.FaultDisputeGameCaller
+}
+
+// NewFaultResponder returns a new [faultResponder].
+func NewFaultResponder(logger log.Logger, txManagr txmgr.TxManager, mpt uint64, fdgAddress *common.Address, client *ethclient.Client) (*faultResponder, error) {
+	fdgContractTransactor, err := bindings.NewFaultDisputeGameTransactor(*fdgAddress, client)
+	if err != nil {
+		return nil, err
+	}
+	fdgContractCaller, err := bindings.NewFaultDisputeGameCaller(*fdgAddress, client)
+	if err != nil {
+		return nil, err
+	}
+	shutdownCtx, cancelShutdownCtx := context.WithCancel(context.Background())
+	return &faultResponder{
+		log:                    logger,
+		shutdownCtx:            shutdownCtx,
+		cancelShutdownCtx:      cancelShutdownCtx,
+		txMgr:                  txManagr,
+		MaxPendingTransactions: mpt,
+		fdgAddr:                fdgAddress,
+		fdgContractTransactor:  fdgContractTransactor,
+		fdgContractCaller:      fdgContractCaller,
+	}, nil
+}
+
+// Quit shuts down the [faultResponder].
+func (r *faultResponder) Quit() {
+	r.log.Info("Shutting down fault responder")
+	r.cancelShutdownCtx()
+}
+
+// buildTxData builds the transaction data for the [faultResponder].
+func (r *faultResponder) buildTxData(ctx context.Context, response Claim) (TxData, error) {
+	// Parent Claim index in the contract claimData array.
+	bigIndex := big.NewInt(int64(response.ParentContractIndex))
+
+	// Build the transaction data using the response claim.
+	if response.DefendsParent() {
+		txdata, err := r.fdgContractTransactor.Defend(&bind.TransactOpts{}, bigIndex, response.ValueBytes())
+		if err != nil {
+			return nil, err
+		}
+		return NewFaultResponderTxData(txdata), nil
+	} else {
+		txdata, err := r.fdgContractTransactor.Attack(&bind.TransactOpts{}, bigIndex, response.ValueBytes())
+		if err != nil {
+			return nil, err
+		}
+		return NewFaultResponderTxData(txdata), nil
+	}
+}
+
+// Respond takes a [Claim] and executes the response action.
+func (r *faultResponder) Respond(ctx context.Context, response Claim) error {
+	// Build the transaction data.
+	txData, err := r.buildTxData(ctx, response)
+	if err != nil {
+		return err
+	}
+
+	// Send the transaction through the txmgr queue.
+	receiptsCh := make(chan txmgr.TxReceipt[TxData])
+	queue := txmgr.NewQueue[TxData](ctx, r.txMgr, r.MaxPendingTransactions)
+	err = sendTransaction(txData, queue, receiptsCh)
+	if err != nil {
+		return err
+	}
+
+	// Block until the transaction receipt is received.
+	r.handleReceipts(ctx, receiptsCh)
+
+	return nil
+}
+
+// handleReceipts handles the transaction receipts.
+// This is a blocking method.
+func (r *faultResponder) handleReceipts(ctx context.Context, receiptsCh chan txmgr.TxReceipt[TxData]) {
+	for {
+		select {
+		case rec := <-receiptsCh:
+			r.log.Info("responder received receipt", "txHash", rec.Receipt.TxHash, "status", rec.Receipt.Status)
+		case <-r.shutdownCtx.Done():
+			r.log.Info("closing responder receipt handler")
+			return
+		}
+	}
+}
+
+// faultResponderTxData
+type faultResponderTxData struct {
+	data []byte
+	dest *common.Address
+}
+
+// NewFaultResponderTxData returns a new [faultResponderTxData].
+func NewFaultResponderTxData(tx *types.Transaction) *faultResponderTxData {
+	return &faultResponderTxData{
+		data: tx.Data(),
+		dest: tx.To(),
+	}
+}
+
+// Bytes returns the transaction data as a byte slice.
+func (txdata *faultResponderTxData) Bytes() []byte {
+	return txdata.data
+}
+
+// Destination returns the destination address of the transaction.
+func (txdata *faultResponderTxData) Destination() *common.Address {
+	return txdata.dest
+}
+
+// TxData is the interface for the data sent in a transaction.
+type TxData interface {
+	// Bytes returns the transaction data as a byte slice.
+	Bytes() []byte
+	// Destination returns the destination address of the transaction.
+	Destination() *common.Address
+}
+
+// TxQueue is an interface for a transaction queue.
+type TxQueue interface {
+	// Send sends a transaction to the queue.
+	Send(txdata TxData, candidate txmgr.TxCandidate, receiptsCh chan txmgr.TxReceipt[TxData])
+}
+
+// sendTransaction creates & submits a transaction using the provided [TxData].
+// Under the hood, it uses the challenger's [txmgr] to handle transaction sending.
+// Gas estimation is performed using go-ethereum's core IntrinsicGas estimation function.
+// This is a blocking method. It should not be called concurrently.
+func sendTransaction(txdata TxData, queue TxQueue, receiptsCh chan txmgr.TxReceipt[TxData]) error {
+	candidate := txmgr.TxCandidate{
+		To:     txdata.Destination(),
+		TxData: txdata.Bytes(),
+		// Setting GasLimit to 0 performs gas estimation online through the [txmgr].
+		GasLimit: 0,
+	}
+	queue.Send(txdata, candidate, receiptsCh)
+
+	return nil
+}
---
+++ op-challenger/fault/responder_test.go
@@ -0,0 +1,40 @@
+package fault
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ethereum-optimism/optimism/op-service/txmgr"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+)
+
+type mockTxData struct {
+	data []byte
+}
+
+func (m *mockTxData) Bytes() []byte {
+	return m.data
+}
+
+func (m *mockTxData) Destination() *common.Address {
+	return &common.Address{}
+}
+
+type mockTxQueue struct{}
+
+func (m *mockTxQueue) Send(txdata TxData, candidate txmgr.TxCandidate, receiptsCh chan txmgr.TxReceipt[TxData]) {
+	receiptsCh <- txmgr.TxReceipt[TxData]{}
+}
+
+// TestResponder_sendTransaction_NoData tests the sendTransaction method
+// with no transaction data provided by the txdata.
+func TestResponder_sendTransaction_NoData(t *testing.T) {
+	receiptChan := make(chan txmgr.TxReceipt[TxData])
+	err := fmt.Errorf("error")
+	go func() {
+		err = sendTransaction(&mockTxData{}, &mockTxQueue{}, receiptChan)
+	}()
+	<-receiptChan
+	require.NoError(t, err)
+}
