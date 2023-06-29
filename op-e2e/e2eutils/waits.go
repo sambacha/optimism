7ae5c331a2dbe10dbc195a7bf6daac0ce68c44e5
Adrian Sutton <adrian@oplabs.co>
2023-06-29 13:39:10 +1000

    op-e2e: Simplify and correct wait period when finalizing withdrawals.

--- op-e2e/e2eutils/waits.go
+++ op-e2e/e2eutils/waits.go
@@ -78,3 +78,23 @@ func WaitFor(ctx context.Context, rate time.Duration, cb func() (bool, error)) e
 		}
 	}
 }
+
+func WaitAndGet[T interface{}](ctx context.Context, pollRate time.Duration, get func() (T, error), predicate func(T) bool) (*T, error) {
+	tick := time.NewTicker(pollRate)
+	defer tick.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-tick.C:
+			val, err := get()
+			if err != nil {
+				return nil, err
+			}
+			if predicate(val) {
+				return &val, nil
+			}
+		}
+	}
+}
